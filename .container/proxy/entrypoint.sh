0a4d22887787913d8ed1f60e65128591a88a70f4
daichi1991 <ks-pan@live.jp>
2024-02-03 11:37:54 +0900

    webの設定修正

--- .container/proxy/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-set -e
-
-nginx -c /app/nginx.conf
--- .container/web/entrypoint.sh
+++ .container/web/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ $JOB != 'true' ]]; then
-  # db:createは初回実行時のみコメントインして実施する
-  bundle exec rails db:create
-  bundle exec rails db:migrate
+if [ "${SECRET_VALUES}" != "" ]; then
+  echo ${SECRET_VALUES} | \
+  jq -r 'to_entries | .[] | "export \(.key)=\"\(.value)\""' > /etc/profile.d/secrets.sh
+  chmod 755 /etc/profile.d/secrets.sh
+  cat /etc/profile.d/secrets.sh >> ~/.bashrc
+  source /etc/profile.d/secrets.sh
 fi
 
-# Exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+bundle exec rails db:migrate
+
+exec bundle exec pumactl start -F ${APP_ROOT}/.container/web/pumaconf.rb
